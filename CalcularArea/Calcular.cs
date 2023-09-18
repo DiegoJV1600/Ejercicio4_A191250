bb189f417dcaca1f3d6f83a512b534d5fcb17478
DiegoJV1600 <diegojuarezdjjv0192@gmail.com>
2023-09-17 20:38:55 -0600

    Se agrega el proyecto

---
+++ CalcularArea/Calcular.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace CalcularArea
+{
+    public class Calcular
+    {
+        public decimal lado { get; set; }
+        public decimal taltura { get; set; }
+        public decimal tbase { get; set; }
+        public double cradio { get; set; }
+        public decimal AreaCuadrado()
+        {
+            return lado * lado;
+        }
+        public decimal AreaTriangulo()
+        {
+            var resultado = tbase * taltura;
+            return resultado / 2;
+        }
+        public double AreaCirculo()
+        {
+            return (cradio * cradio) * Math.PI;
+        }
+    }
+}
---
+++ CalcularArea/CalcularArea.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalcularArea
+{
+    /// <summary>
+    /// Descripción breve de CalcularArea
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularArea : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos, mi nombre es Juan Diego";
+        }
+        [WebMethod]
+        public decimal AreaCuadrado(decimal lado)
+        {
+            Calcular cala = new Calcular();
+            cala.lado = lado;  
+            return cala.AreaCuadrado();
+        }
+        [WebMethod]
+        public decimal AreaTriangulo(decimal bas, decimal alt)
+        {
+            Calcular cala = new Calcular();
+            cala.tbase = bas;
+            cala.taltura = alt;
+            return cala.AreaTriangulo();
+        }
+        [WebMethod]
+        public double AreaCirculo(double radio)
+        {
+            Calcular cala = new Calcular();
+            cala.cradio = radio;
+            return cala.AreaCirculo();
+        }
+    }
+}
